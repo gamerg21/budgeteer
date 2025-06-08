b4d9aa8383279b8af5c652e27a579745da4c3b21
George A Vina <george@georgevina.com>
2025-06-07 21:57:39 -0400

    Initial commit of Budgeteer backend

---
+++ app/__init__.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+
+db = SQLAlchemy()
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    db.init_app(app)
+    CORS(app)
+
+    from .routes import api
+    app.register_blueprint(api)
+
+    return app
